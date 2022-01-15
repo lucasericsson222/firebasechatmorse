849640660aaddd097372d7b9a9f20488f6f333c6
skyfire222 <skyfire222@users.noreply.github.com>
2022-01-14 19:52:31 -0800

    firebbase

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import { ChatEngine} from 'react-chat-engine';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const App = () => {
+    return (
+        <ChatEngine
+            height ="100vh"
+            projectID="579088c7-d15e-4b61-bed6-0eb92415d115"
+            userName="javascriptmastery"
+            userSecret="123123"
+        />
+    )
 }
-
 export default App;
